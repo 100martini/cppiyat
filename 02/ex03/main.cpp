6c0b3e39ef08f9cfa0bf51d6977e9957f8d1b9ac
Wiam El Kassah <wel-kass@e1r4p10.1337.ma>
2025-05-01 15:40:39 +0100

    done

--- 02/ex03/main.cpp
+++ 02/ex03/main.cpp
@@ -11,10 +11,10 @@ int main(void) {
     Point edge(0.0f, 5.0f);
     Point vertex(0.0f, 0.0f);
     
-    std::cout << "Point (1, 1) is inside the triangle: " << (bsp(a, b, c, inside) ? "true" : "false") << std::endl;
-    std::cout << "Point (20, 20) is inside the triangle: " << (bsp(a, b, c, outside) ? "true" : "false") << std::endl;
-    std::cout << "Point (0, 5) is inside the triangle: " << (bsp(a, b, c, edge) ? "true" : "false") << std::endl;
-    std::cout << "Point (0, 0) is inside the triangle: " << (bsp(a, b, c, vertex) ? "true" : "false") << std::endl;
+    std::cout << "Point (1, 1) is inside the triangle: " << (bsp(a, b, c, inside) ? "yes" : "NOPE") << std::endl;
+    std::cout << "Point (20, 20) is inside the triangle: " << (bsp(a, b, c, outside) ? "yes" : "NOPE") << std::endl;
+    std::cout << "Point (0, 5) is inside the triangle: " << (bsp(a, b, c, edge) ? "yes" : "NOPE") << std::endl;
+    std::cout << "Point (0, 0) is inside the triangle: " << (bsp(a, b, c, vertex) ? "yes" : "NOPE") << std::endl;
     
     return 0;
 }
